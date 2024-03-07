d877c769fdee788b5ba9306e78fa6c4b2c94dbc9
ChenHaoJie9527 <a18520145493@163.com>
2024-03-07 14:58:47 +0800

    feat: 将僵尸分配给调用者

--- contracts/ZombieFactory2.sol
+++ contracts/ZombieFactory2.sol
@@ -21,6 +21,8 @@ contract ZombieFactory2 {
     function _createZombie(string calldata _name, uint256 _dna) private {
         uint256 id = zombies.length;
         zombies.push(Zombie(_name, _dna));
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
