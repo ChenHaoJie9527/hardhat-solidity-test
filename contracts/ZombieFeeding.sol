816bb1105c69339fac10fc43a2705a72bf6327c2
ChenHaoJie9527 <a18520145493@163.com>
2024-03-07 17:09:56 +0800

    feat: 将自身僵尸dna与猎物生物dna结合形成新的僵尸dna

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -3,4 +3,12 @@ pragma solidity ^0.8.20;
 
 import "./ZombieFactory2.sol";
 
-contract ZombieFeeding is ZombieFactory2 {}
+contract ZombieFeeding is ZombieFactory2 {
+
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        // 判断调用函数者是否是自己
+        require(zombieToOwner[_zombieId] == msg.sender);
+        // 获取属于自身的僵尸
+        Zombie storage myZombie = zombies[_zombieId];
+    }
+}
