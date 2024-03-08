c1527cd71e81493088eb2490e698bfc1a9eafb74
ChenHaoJie9527 <a18520145493@163.com>
2024-03-08 15:09:41 +0800

    feat: 处理函数多个返回值

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -29,6 +29,7 @@ abstract contract KittyInterface {
 contract ZombieFeeding is ZombieFactory2 {
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     KittyInterface kittyContract = KittyInterface(ckAddress);
+
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         // 判断调用函数者是否是自己
         require(zombieToOwner[_zombieId] == msg.sender);
@@ -38,5 +39,11 @@ contract ZombieFeeding is ZombieFactory2 {
         uint newDna = (myZombie.dna + _targetDna) / 2;
         string memory isName = "name1";
         _createZombie(isName, newDna);
+    }
+
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (, , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna);
     }
 }
