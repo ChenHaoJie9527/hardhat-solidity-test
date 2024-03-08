997fc8e618f4e003433d402935efef78bcf72af3
ChenHaoJie9527 <a18520145493@163.com>
2024-03-08 17:17:02 +0800

    feat: 部署合约

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("./task/index.js");
+require("@nomicfoundation/hardhat-ignition-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
