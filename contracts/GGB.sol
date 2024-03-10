171af944f7053e845deea9818e650c068d6a89f1
ChenHaoJie9527 <a18520145493@163.com>
2024-03-10 17:28:16 +0800

    feat: 部署ggb合约到x1链

--- contracts/GGB.sol
+++ contracts/GGB.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
-contract BBG {
+contract GGB {
     uint count;
     string name;
 
--- scripts/ggb.js
+++ scripts/ggb.js
@@ -1,13 +1,21 @@
 const hre = require("hardhat");
 
 async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+
+  const lockedAmount = hre.ethers.parseEther("0.001");
   const ggbContract = await hre.ethers.getContractFactory("GGB");
   const ggb = await ggbContract.deploy();
-  await ggb.waitForDeployment();
-  console.log("GGB deployed to:", ggb.address);
+  const ggbTarget = await ggb.waitForDeployment();
+  console.log(
+    `Lock with ${hre.ethers.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${ggbTarget.target}`
+  );
 }
 
-main().catch(() => {
+main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
