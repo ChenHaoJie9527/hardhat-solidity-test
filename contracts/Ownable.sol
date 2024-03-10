c4ae2d858ba008803d3275b39a9d040fca8c837d
ChenHaoJie9527 <a18520145493@163.com>
2024-03-10 19:15:48 +0800

    feat: Ownable合约

---
+++ contracts/Ownable.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+contract Ownable {
+    address public owner;
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOner
+    );
+
+    function isOwnable() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
