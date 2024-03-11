5365ae6b17faa1a858a77ab12d48d290c34c8fef
ChenHaoJie9527 <a18520145493@163.com>
2024-03-11 16:55:25 +0800

    feat: 鼓励机制

--- contracts/Zombiehelper.sol
+++ contracts/Zombiehelper.sol
@@ -9,4 +9,20 @@ contract zombiehelper is ZombieFeeding {
         require(zombies[_zombieId].level >= _level);
         _;
     }
+
+    function changeName(
+        uint _zombieId,
+        string calldata _newName
+    ) external aboveLevel(2, _zombieId) {
+        require(zombieToOwner[_zombieId] == msg.sender);
+        zombies[_zombieId].name = _newName;
+    }
+
+    function changeNna(
+        uint _zombieId,
+        uint _newDna
+    ) external aboveLevel(20, _newDna) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    }
 }
